083cf0831b19e5760e4835cae64e37ea14459a47
yehudashor <72968067+yehudashor@users.noreply.github.com>
2022-05-07 23:45:06 +0300

    Initial commit

---
+++ src/DirectoryDetails.java
@@ -0,0 +1,11 @@
+// TODO: Implement Composite (change this file).
+public class DirectoryDetails extends FileDetails {
+    public DirectoryDetails(String path, String name){
+        super(path,name);
+    }
+
+
+    public void addFile(FileDetails fileDetails){
+        // TODO: complete
+    }
+}
