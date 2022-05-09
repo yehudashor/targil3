8c3b937a699bbb5157d41e8a0284874faaf13861
davidovits1 <david.o3111@gmail.com>
2022-05-09 22:11:59 +0300

    function of statics

--- src/VisitorStatistics.java
+++ src/VisitorStatistics.java
@@ -1,4 +1,64 @@
-public class VisitorStatistics implements Visitor{
+import java.util.HashMap;
+import java.util.Map;
+
+public class VisitorStatistics implements Visitor {
+
+    private double bytesPerSecondMp3;
+    private double bytesPerPixelJpg;
+    private long countOfWordsTxt;
+    private long countOfLineHtml;
+    private long sizeOfPptx;
+    private long slidesOfPptx;
+    public Map <String, Integer> CountOfFilesInFolders = new HashMap<>();
+
+    public long getCountOfLine() {
+        return countOfLineHtml;
+    }
+
+    private void setCountOfLine(long _countOfLine) {
+        countOfLineHtml += _countOfLine;
+    }
+
+    public long AverageSizeSlide(){
+        return sizeOfPptx / slidesOfPptx;
+    }
+
+    private long round(double number) {
+        return Math.round(number);
+    }
+
+    private void setBytesPerSecondMp3(double _bytesPerSecondMp3) {
+        bytesPerSecondMp3 += _bytesPerSecondMp3;
+    }
+
+    public long GetBytesPerSecondMp3() {
+        return round(bytesPerSecondMp3);
+    }
+
+    private void setBytesPerSecondPixel(double _bytesPerPixel) {
+        bytesPerPixelJpg += _bytesPerPixel;
+    }
+
+    public long GetBytesPerSecondPixel() {
+        return round(bytesPerPixelJpg);
+    }
+
+    private void setCountOfWords(int _countOfWords) {
+        countOfWordsTxt += _countOfWords;
+    }
+
+    public long GetCountOfWords() {
+        return countOfWordsTxt;
+    }
+
+    private void setSizeOfPptx(int _sizeOfPptx ){
+        sizeOfPptx += _sizeOfPptx;
+    }
+
+    private void setSlidesOfPptx(int _slidesOfPptx ){
+        slidesOfPptx += _slidesOfPptx;
+    }
+
 
     @Override
     public void visitor(DirectoryDetails directoryDetails) {
@@ -12,26 +72,27 @@ public class VisitorStatistics implements Visitor{
 
     @Override
     public void visitor(HtmlFileDetails htmlFileDetails) {
-
+        setCountOfLine(htmlFileDetails.getLines());
     }
 
     @Override
     public void visitor(JpgFileDetails jpgFileDetails) {
-
+        setBytesPerSecondPixel(jpgFileDetails.getSize() / jpgFileDetails.getHeight() * jpgFileDetails.getWidth());
     }
 
     @Override
     public void visitor(Mp3FileDetails mp3FileDetails) {
-
+        setBytesPerSecondMp3(mp3FileDetails.getSize() / mp3FileDetails.getLengthSec());
     }
 
     @Override
     public void visitor(PptxFileDetails pptxFileDetails) {
-
+        setSizeOfPptx(pptxFileDetails.getSize());
+        setSlidesOfPptx(pptxFileDetails.getSlides());
     }
 
     @Override
     public void visitor(TxtFileDetails txtFileDetails) {
-
+       setCountOfWords(txtFileDetails.getWords());
     }
 }
